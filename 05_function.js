124c781a66813faaef43c9a61a9f121ba858e4b0
v000211v <v000211v@naver.com>
2020-10-22 11:04:19 +0900

    javascript

---
+++ 05_function.js
@@ -0,0 +1,14 @@
+// 1. function
+// - 함수선언과 함수호출
+function myFunc( ) {
+  console.log('안녕, 자바스크립트');
+}
+
+myFunc()
+const a = 10
+
+const sayHello = function() {
+  console.log('Hello');
+}
+
+sayHello ()
--- app.js
+++ app.js
@@ -1,2 +1,9 @@
-const age = 15
-console.log(age > 20 ? `성인` :`청소년`);
+const input = document.querySelector('.input')
+const btn = document.querySelector('.btn')
+const result = document.querySelector('.result')
+
+btn.addEventListener('click', function(e ) {
+  e.preventDefault()
+  const value = input.value
+  result.textContent = value
+})
