47580df87d67496e14cc5d2341509d72b473dc64
v000211v <v000211v@naver.com>
2020-10-26 10:44:12 +0900

    java

--- 06_object.js
+++ 06_object.js
@@ -1,53 +1,78 @@
 // 오브젝트 object
 // - 자바스크립트 데이터
 // - 상태와 행동을 가진다.
-// phone의 상태 (properties) -> color, size, 
-// phone의 행동 (methods) -> ring, take a picture, play music  
+// phone의 상태 (properties) -> color, size,
+// phone의 행동 (methods) -> ring, take a picture, play music
 // 형식 { 키 : 값 }
 
 // 1. object
 
 const user = {
-  name : 'Picaso',
-  age : 30,
-  email : 'picaso@gmail.com',
-  works : [
-    {id:1, title: '게르니카', price: 10000, like:50},
-    {id:2, title: '아비뇽의 처녀들', price: 20000, like:80},
-    {id:3, title: '우는 여인', price: 50000, like:200}
+  name: 'Picaso',
+  age: 30,
+  email: 'picaso@gmail.com',
+  works: [
+    { id: 1, title: '게르니카', price: 10000, like: 50 },
+    { id: 2, title: '아비뇽의 처녀들', price: 20000, like: 80 },
+    { id: 3, title: '우는 여인', price: 50000, like: 200 }
   ],
   // login : function () {
   //   console.log('user logged in');
   // }
-  
-  
+
   login() {
-    console.log('user logged in');
+    console.log('user logged in')
   },
 
   logout() {
-    console.log('user logged out');
+    console.log('user logged out')
   },
 
-  goodworks : function() {
-    console.log('피카소의 대표작');
-    this.works.forEach(work => {
+  goodworks: function () {
+    console.log('피카소의 대표작')
+    this.works.forEach((work) => {
       console.log(
-        `${work.id}, ${work.title}, ${work.price}원, 좋아요${work.like}`);
+        `${work.id}, ${work.title}, ${work.price}원, 좋아요${work.like}`
+      )
     })
   }
 }
 
-console.log(user.age);
+console.log(user.age)
 
 //수정
 user.age = 45
-console.log(user.age);
+console.log(user.age)
 
 //타입 확인
-console.log(typeof user);
+console.log(typeof user)
 
 //methods
 user.login()
 user.logout()
 user.goodworks()
+
+// 2. Math object
+console.log(Math)
+console.log(Math.PI)
+
+const sum = 9.7
+console.log(Math.round(sum))
+console.log(Math.floor(sum))
+console.log(Math.ceil(sum))
+console.log(parseInt(sum))
+
+// random number
+const num = Math.random()
+console.log(num)
+
+// 0 ~ 9 사이의 정수출력
+console.log(Math.floor(Math.random() * 10))
+
+// 1 ~ 10 사이의 정수출력
+console.log(Math.floor(Math.random() * 10) + 1)
+
+//배열 랜덤 출력
+const food = ['라면', '김밥', '우동']
+const randomNum = Math.floor(Math.random() * food.length)
+console.log(food[randomNum])
---
+++ 08_array.js
@@ -0,0 +1,20 @@
+// Array 배열
+
+// 1. 배열 만들기
+const arr1 = new Array()
+console.log(arr1)
+
+const arr2 = [1, 3, 5, 7, 9]
+console.log(arr2)
+
+const arr3 = ['what', 'are', 'you', 'doing']
+console.log(arr3)
+
+const arr4 = [
+  'a',
+  { model: 'apple', price: 1000 },
+  { model: 'galaxy', price: 2000 }
+]
+
+console.log(arr4)
+console.log(arr4[1].model)
