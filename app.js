84fcee45cb5a8a1f6b7c1df35140e4542758c0d9
v000211v <v000211v@naver.com>
2020-10-26 10:59:41 +0900

    java

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const input = document.querySelector('.input')
-const btn = document.querySelector('.btn')
-const result = document.querySelector('.result')
+// 구구단 출력
+for (let i = 2; i < 10; i++) {
+  console.log(`${i}단`)
+  console.log('----------------------')
 
-btn.addEventListener('click', function(e ) {
-  e.preventDefault()
-  const value = input.value
-  result.textContent = value
-})
+  for (let j = 1; j < 10; j++) {
+    console.log(`${i} x ${j} = ${i * j}`)
+  }
+  console.log('---------------------')
+}
