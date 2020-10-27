cb68012cbe918aeb821810e2eebeea1b004b8036
v000211v <v000211v@naver.com>
2020-10-27 09:50:14 +0900

    java

--- 08_array.js
+++ 08_array.js
@@ -18,3 +18,55 @@ const arr4 = [
 
 console.log(arr4)
 console.log(arr4[1].model)
+
+// 2. 배열 출력
+const books = ['html', 'css', 'javascript']
+console.log(books)
+console.log(books.length)
+console.log(books[0])
+console.log(books[books.length - 1])
+
+console.clear()
+// 3. Loop 출력
+// - for문
+
+for (let i = 0; i < books.length; i++) {
+  console.log(books[i])
+}
+
+// - for of
+for (let book of books) {
+  console.log(book)
+}
+
+// forEach
+books.forEach((book) => console.log(book + ' hello'))
+
+// 4. 추가, 삭제
+// - 뒤에서 추가
+books.push('jquery')
+console.log(books)
+
+books.push('vue')
+console.log(books)
+
+// - 앞에서 추가
+books.unshift('react')
+console.log(books)
+
+// - 뒤에서 삭제
+books.pop()
+console.log(books)
+
+// - 앞에서 삭제
+books.shift()
+console.log(books)
+
+// - splice : 지정한 위치에서 추가, 삭제
+books.splice(2, 1, 'react', 'vue')
+console.log(books)
+
+// - 두개의 배열을 합치기
+const books2 = ['git', 'nodejs']
+const newBook = books.concat(books2)
+console.log(newBook)
